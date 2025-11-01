e6d28d2d6c649e45709415c5946d514572ea8b8c
Serhii <grey.egorov@gmail.com>
2025-11-01 12:34:27 +0200

    Update backend configuration for S3 bucket and region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-jkbskefbjek"
+    bucket       = "demo-s3-test-2025"
     key          = "terraform.tfstate"
-    region       = "eu-central-1"
+    region       = "us-east-1"
     use_lockfile = true
     encrypt      = true
   }
