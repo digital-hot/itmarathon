54c50cdfb348a90b55b018783a6b42543e27dc8e
Serhii <grey.egorov@gmail.com>
2025-11-06 16:50:08 +0200

    feat: update Angular version and add delete user functionality
    - Updated Angular version in package-lock.json from 0.1.1 to 0.1.3.
    - Added a new "trash" icon to the icons sprite.
    - Introduced deleteUser method in ApiService to handle user deletion.
    - Implemented deleteUser method in UserService to call the API and manage user state.
    - Enhanced ParticipantCard component to include delete functionality for users.
    - Updated enums to include new icon and aria labels for delete actions.

--- frontend/angular/src/app/app.enum.ts
+++ frontend/angular/src/app/app.enum.ts
@@ -19,6 +19,7 @@ export const enum IconName {
   Close = 'close',
   Link = 'link',
   Info = 'info',
+  Delete = 'trash',
 }
 
 export const enum AriaLabel {
@@ -31,6 +32,7 @@ export const enum AriaLabel {
   Close = 'Close',
   ParticipantLink = 'Copy personal link',
   Info = 'Info',
+  Delete = 'Delete user',
 }
 
 export const enum InputSidebarText {
@@ -120,6 +122,7 @@ export enum ToastMessage {
   PleaseCreateYourRoom = 'Please create your room first.',
   PleaseJoinTheRoom = 'Please join the room first',
   SuccessDrawNames = 'Success! All participants are matched.\nLet the gifting magic start!',
+  SuccessDeleteUser = 'User has been successfully deleted.',
 }
 
 export const enum InputType {
